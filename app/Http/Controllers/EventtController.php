8af314439cb887729e45e62750a3dce09187c4b0
ibra-khm <ibrahimkhamaiseh1@gmail.com>
2022-10-20 16:21:29 +0300

    first push

---
+++ app/Http/Controllers/EventtController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Eventt;
+use Illuminate\Http\Request;
+
+class EventtController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Eventt  $eventt
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Eventt $eventt)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Eventt  $eventt
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Eventt $eventt)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Eventt  $eventt
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Eventt $eventt)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Eventt  $eventt
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Eventt $eventt)
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_10_20_121443_create_eventts_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('eventts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->longText('description');
+            $table->string('location')->nullable();
+            $table->string('tags')->nullable();
+            $table->dateTime('date')->nullable();
+            $table->string('city')->nullable();
+            $table->string('duration')->nullable();
+            $table->integer('capacity')->nullable();
+            $table->binary('thumbnail')->nullable();
+            $table->binary('banner')->nullable();
+            $table->boolean('publish')->default('1');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('eventts');
+    }
+};
